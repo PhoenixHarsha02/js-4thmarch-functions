6b936dd5f516443966b13f95673e4c93cd7575a0
PhoenixHarsha02 <harshastrikes007@gmail.com>
2020-03-04 19:28:50 +0530

    1 file modified

--- index.js
+++ index.js
@@ -49,6 +49,69 @@ function compareas(a,b){
 compare(ab,bc);
 //Comparevalues
 
-let x
-function mark(x)
 
+function grade(v){
+  if(v>90){
+    return "S"
+  }
+if(v>60 && v<90){
+  return "First Class"
+}
+if(v<35){
+  return "Attempt"
+}
+else{
+  return "Second Class"
+}}
+let x=grade(80);
+console.log(x);
+//Grade 
+//Output:First Class
+
+function vowels(r){
+  switch(r){
+    case 'A','a':
+        console.log("Vowel");
+        break;
+    case 'E','e':
+        console.log("Vowel");
+        break;
+    case 'I','i':
+        console.log("Vowel");
+        break;
+    case 'O','o':
+        console.log("Vowel");
+        break;
+    case 'U','u':
+        console.log("Vowel");
+        break;
+    default:
+        console.log("Consonant");
+  }
+}
+let t=vowels(a);
+console.log(t);
+//vowels
+
+function prime(w)
+{
+  if (w===1)
+  {
+    return false;
+  }
+  else if(w === 2)
+  {
+    return true;
+  }else
+  {
+    for(var o = 2; o < w; o++)
+    {
+      if(w % o === 0)
+      {
+        return false;
+      }
+    }
+    return true;  
+  }
+}
+let y=prime(20);
